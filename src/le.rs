c14623b1c271b783639268f53eac37a4625a7d20
John Turner <jturner.usa@gmail.com>
2025-01-08 23:03:13 -0500

    use macro to define little endian primitive wrappers

--- src/le.rs
+++ src/le.rs
@@ -1,33 +1,24 @@
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct U64(u64);
+macro_rules! le {
+    ($struct:tt, $t:ty) => {
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        pub struct $struct($t);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct U8(u8);
+        impl $struct {
+            pub fn new(i: $t) -> Self {
+                if cfg!(target_endian = "little") {
+                    Self(i)
+                } else {
+                    Self(<$t>::from_le_bytes(<$t>::to_le_bytes(i)))
+                }
+            }
 
-impl U64 {
-    pub fn new(i: u64) -> Self {
-        if cfg!(target_endian = "little") {
-            Self(i)
-        } else {
-            Self(u64::from_le_bytes(u64::to_le_bytes(i)))
+            pub fn get(self) -> $t {
+                self.0
+            }
         }
-    }
-
-    pub fn get(self) -> u64 {
-        self.0
-    }
+    };
 }
 
-impl U8 {
-    pub fn new(i: u8) -> Self {
-        if cfg!(target_endian = "little") {
-            Self(i)
-        } else {
-            Self(u8::from_le_bytes(u8::to_le_bytes(i)))
-        }
-    }
-
-    pub fn get(self) -> u8 {
-        self.0
-    }
-}
+le!(U64, u64);
+le!(U32, u32);
+le!(U16, u16);
